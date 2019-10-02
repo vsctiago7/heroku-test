e4980ca3f4a84be63f956f773b4b6a0a34286c88
Tiago Fernandes <vsctiago7@gmail.com>
2019-10-02 20:16:45 +0100

    changed app name and start script

--- src/bin/www.js
+++ src/bin/www.js
@@ -7,7 +7,7 @@
 import app from '../app';
 import debugLib from 'debug';
 import http from 'http';
-const debug = debugLib('express-backend-boilerplate:server');
+const debug = debugLib('heroku-test:server');
 
 
 /**

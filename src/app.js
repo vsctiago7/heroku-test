6ce6675bc38d02f70723ca39efa09eef735e679f
Tiago Fernandes <vsctiago7@gmail.com>
2019-10-02 22:07:22 +0100

    Added cors and permitted origin fercodes netlify client app.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
 
-import indexRouter from './routes/index';
+import indexRouter from "./routes/index";
 
 const app = express();
 
-app.use(logger('dev'));
+const corsOptions = {
+  origin: "https://fercodes.netlify.com/"
+}
+
+app.use(logger("dev"));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 export default app;
